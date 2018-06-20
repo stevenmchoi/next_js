9bbad89177ef63d5469767f040997bd4a711d3b2
Steven Choi <steven.m.choi@gmail.com>
2018-06-20 02:24:53 -0700

    Add styles

--- components/main.js
+++ components/main.js
@@ -3,6 +3,18 @@ import Link from 'next/link';
 const Menu = ( { href, label } ) => (
 	<span>
 		<Link href={href}><a>{label}</a></Link>
+
+		<style jsx>{`
+			{
+				padding: 6px 4px;
+			};
+
+			a {
+				color: #fff;
+				text-decoration: none;
+				font-size: 16px;
+			};
+		`}</style>
 	</span>
 );
 
@@ -20,8 +32,40 @@ export default ( { children } ) => (
 				) )
 			}
 		</nav>
+
 		<div>
 			{children}
 		</div>
+
+		<style global jsx>{`
+			body {
+				font-family: Helvetica, sans-serif;
+			};
+
+			nav {
+				text-align: left;
+				background-color: #000;
+				color: #fff;
+				padding: 12px;
+				position: fixed;
+				z-index: 1000;
+				top: 0;
+				left: 0;
+				right: 0;
+			};
+
+			a {
+				color: #0e0e0e;
+				text-decoration: none;
+			};
+		`}</style>
+
+		<style jsx>{`
+			div {
+				width: 100%;
+				color: #333;
+				padding-top: 30px;
+			};
+		`}</style>
 	</main>
 );
