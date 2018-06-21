f62a9af8c6cd0ec3e073432730c92a4bf50d6103
Steven Choi <steven.m.choi@gmail.com>
2018-06-21 16:09:28 -0700

    Move html head and body tags to 'main' component and delete 'about' page

--- components/main.js
+++ components/main.js
@@ -19,46 +19,60 @@ const Menu = ( { href, label } ) => (
 );
 
 export default ( { children } ) => (
-	<main>
-		<nav>
-			<Menu href={'/'} label={'Home'} />
-			<Menu key='employees' href={'/employees'} label={'Employees'} />
-			<Menu key='about' href={'/about'} label={'About'} />
-		</nav>
+	<html lang="en">
+		<head>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossOrigin="anonymous" />
 
-		<div>
-			{children}
-		</div>
+			<title>Employee Paystubs</title>
+		</head>
+		<body>
+			<main>
+				<nav>
+					<Menu href={'/'} label={'Home'} />
+					<Menu key='employees' href={'/employees'} label={'Employees'} />
+				</nav>
 
-		<style global jsx>{`
-			body {
-				font-family: Helvetica, sans-serif;
-			};
+				<div>
+					{children}
+				</div>
 
-			nav {
-				text-align: left;
-				background-color: #000;
-				color: #fff;
-				padding: 12px;
-				position: fixed;
-				z-index: 1000;
-				top: 0;
-				left: 0;
-				right: 0;
-			};
+				<style global jsx>{`
+					body {
+						font-family: Helvetica, sans-serif;
+					};
 
-			a {
-				color: #0e0e0e;
-				text-decoration: none;
-			};
-		`}</style>
+					nav {
+						text-align: left;
+						background-color: #000;
+						color: #fff;
+						padding: 12px;
+						position: fixed;
+						z-index: 1000;
+						top: 0;
+						left: 0;
+						right: 0;
+					};
 
-		<style jsx>{`
+					a {
+						color: #0e0e0e;
+						text-decoration: none;
+					};
+				`}</style>
+
+				<style jsx>{`
 			div {
 				width: 100%;
 				color: #333;
 				padding-top: 50px;
 			};
 		`}</style>
-	</main>
+			</main>
+
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossOrigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossOrigin="anonymous"></script>
+		</body>
+	</html>
 );
--- pages/employees.js
+++ pages/employees.js
@@ -1,26 +1,29 @@
 import { EmployeeContext } from "../components/employee_context";
+import Page from "../components/main";
 
 export default () => (
-	<EmployeeContext.Consumer>
-		{( employees ) => {
-			console.log( employees );
+	<Page>
+		<EmployeeContext.Consumer>
+			{( employees ) => {
+				console.log( employees );
 
-			return (
-				<div className="page">
-					<table className="employee-table">
-						<thead>
-							<tr>
-								<th scope="col">Employee Name</th>
-								<th scope="col">Pay Period</th>
-								<th scope="col">Gross Income</th>
-								<th scope="col">Income Tax</th>
-								<th scope="col">Net Income</th>
-								<th scope="col">Super Amount</th>
-							</tr>
-						</thead>
-					</table>
-				</div>
-			);
-		}}
-	</EmployeeContext.Consumer>
+				return (
+					<div className="page">
+						<table className="employee-table">
+							<thead>
+								<tr>
+									<th scope="col">Employee Name</th>
+									<th scope="col">Pay Period</th>
+									<th scope="col">Gross Income</th>
+									<th scope="col">Income Tax</th>
+									<th scope="col">Net Income</th>
+									<th scope="col">Super Amount</th>
+								</tr>
+							</thead>
+						</table>
+					</div>
+				);
+			}}
+		</EmployeeContext.Consumer>
+	</Page>
 );
