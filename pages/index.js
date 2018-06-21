37de6a730c762f244abe6420a1f178eeee64f0c5
Steven Choi <steven.m.choi@gmail.com>
2018-06-21 10:50:30 -0700

    Indent input code

--- pages/index.js
+++ pages/index.js
@@ -13,31 +13,31 @@ export default class extends Component {
 						<h1>Employee</h1>
 						<form className="employee-form">
 							<label>First Name:
-					<input type="text" />
+								<input type="text" />
 							</label>
 
 							<br />
 
 							<label>Last Name:
-					<input type="text" />
+								<input type="text" />
 							</label>
 
 							<br />
 
 							<label>Annual Salary (natural numbers):
-					<input type="text" />
+								<input type="text" />
 							</label>
 
 							<br />
 
 							<label>Super Rate (0% - 12% inclusive):
-					<input type="text" />
+								<input type="text" />
 							</label>
 
 							<br />
 
 							<label>Pay Period Start Date (e.g. 01 - March):
-					<input type="text" />
+								<input type="text" />
 							</label>
 
 							<br />
