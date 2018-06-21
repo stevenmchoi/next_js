7c18cb2c698b0334a63dfd43da126957af95f22d
Steven Choi <steven.m.choi@gmail.com>
2018-06-21 10:55:33 -0700

    Create preloaded employee themes for outputs

---
+++ components/theme_context.js
@@ -0,0 +1,18 @@
+export const employee_themes = {
+	a_smith: {
+		employeeName: "Andrew Smith",
+		payPeriod: "01 March - 31 March",
+		grossIncome: 5004,
+		incomeTax: 922,
+		netIncome: 4082,
+		superAmount: 450
+	},
+	c_wong: {
+		employeeName: "Claire Wong",
+		payPeriod: "01 March - 31 March",
+		grossIncome: 10000,
+		incomeTax: 2669,
+		netIncome: 7331,
+		superAmount: 1000
+	}
+};
