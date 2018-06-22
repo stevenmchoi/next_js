7e789db1523afe0be609915470604693f3f338e6
Steven Choi <steven.m.choi@gmail.com>
2018-06-21 18:12:31 -0700

    Create onChange and onSubmit handlers

--- pages/employee_form.js
+++ pages/employee_form.js
@@ -2,39 +2,88 @@ import { Component } from "react";
 import { EmployeeContext } from '../components/employee_context'
 
 class EmployeeForm extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = {
+			firstName: "",
+			lastName: "",
+			annualSalary: "",
+			superRate: "",
+			payPeriodStartDate: ""
+		}
+
+		this.handleChange = this.handleChange.bind( this );
+		this.handleSubmit = this.handleSubmit.bind( this );
+	}
+
+	handleChange( event ) {
+		this.setState( {
+			[ event.target.name ]: event.target.value
+		} );
+	}
+
+	handleSubmit( event ) {
+		event.preventDefault();
+	}
+
 	render() {
 		return (
 			<EmployeeContext.Consumer>
 				{( employees ) => (
 					<div className="page">
 						<h1>Employee</h1>
-						<form className="employee-form">
+						<form className="employee-form" onSubmit={this.handleSubmit}>
 							<label>First Name:
-								<input type="text" />
+								<input
+									type="text"
+									name="firstName"
+									value={this.state.firstName}
+									onChange={this.handleChange}
+								/>
 							</label>
 
 							<br />
 
 							<label>Last Name:
-								<input type="text" />
+								<input
+									type="text"
+									name="lastName"
+									value={this.state.lastName}
+									onChange={this.handleChange}
+								/>
 							</label>
 
 							<br />
 
 							<label>Annual Salary (natural numbers):
-								<input type="text" />
+								<input
+									type="text"
+									name="annualSalary"
+									value={this.state.annualSalary}
+									onChange={this.handleChange}
+								/>
 							</label>
 
 							<br />
 
 							<label>Super Rate (0% - 12% inclusive):
-								<input type="text" />
+								<input
+									type="text"
+									name="superRate"
+									value={this.state.superRate}
+									onChange={this.handleChange}
+								/>
 							</label>
 
 							<br />
 
 							<label>Pay Period Start Date (e.g. 01 - March):
-								<input type="text" />
+								<input
+									type="date"
+									name="payPeriodStartDate"
+									value={this.state.payPeriodStartDate}
+									onChange={this.handleChange}
+								/>
 							</label>
 
 							<br />
