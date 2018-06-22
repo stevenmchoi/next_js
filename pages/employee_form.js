3e4622beca1309abcf52c5ed18a5c9e18e9ac011
Steven Choi <steven.m.choi@gmail.com>
2018-06-21 17:58:04 -0700

    Make EmployeeForm into a class

--- pages/employee_form.js
+++ pages/employee_form.js
@@ -1,44 +1,50 @@
+import { Component } from "react";
 import { EmployeeContext } from '../components/employee_context'
 
-export default () => (
-	<EmployeeContext.Consumer>
-		{( employees ) => (
-			<div className="page">
-				<h1>Employee</h1>
-				<form className="employee-form">
-					<label>First Name:
-				<input type="text" />
-					</label>
+class EmployeeForm extends Component {
+	render() {
+		return (
+			<EmployeeContext.Consumer>
+				{( employees ) => (
+					<div className="page">
+						<h1>Employee</h1>
+						<form className="employee-form">
+							<label>First Name:
+								<input type="text" />
+							</label>
 
-					<br />
+							<br />
 
-					<label>Last Name:
-				<input type="text" />
-					</label>
+							<label>Last Name:
+								<input type="text" />
+							</label>
 
-					<br />
+							<br />
 
-					<label>Annual Salary (natural numbers):
-				<input type="text" />
-					</label>
+							<label>Annual Salary (natural numbers):
+								<input type="text" />
+							</label>
 
-					<br />
+							<br />
 
-					<label>Super Rate (0% - 12% inclusive):
-				<input type="text" />
-					</label>
+							<label>Super Rate (0% - 12% inclusive):
+								<input type="text" />
+							</label>
 
-					<br />
+							<br />
 
-					<label>Pay Period Start Date (e.g. 01 - March):
-				<input type="text" />
-					</label>
+							<label>Pay Period Start Date (e.g. 01 - March):
+								<input type="text" />
+							</label>
 
-					<br />
+							<br />
 
-					<button>Enter</button>
-				</form>
-			</div>
-		)}
-	</EmployeeContext.Consumer>
-);
+							<button>Enter</button>
+						</form>
+					</div>
+				)}
+			</EmployeeContext.Consumer>
+		);
+	}
+}
+export default EmployeeForm;
