218d15cf30ddd99ea2e6bc3d4c0b7fc811b6dd79
Steven Choi <steven.m.choi@gmail.com>
2018-06-22 04:37:10 -0700

    Separate Menu as another component

--- components/main.js
+++ components/main.js
@@ -1,25 +1,7 @@
 import { Component } from "react";
-import Link from 'next/link';
+import Menu from "./menu";
 
 import { EmployeeContext, employees } from '../components/employee_context'
-
-const Menu = ( { href, label } ) => (
-	<span>
-		<Link href={href}><a>{label}</a></Link>
-
-		<style jsx>{`
-			{
-				padding: 6px 4px;
-			};
-
-			a {
-				color: #fff;
-				text-decoration: none;
-				font-size: 16px;
-			};
-		`}</style>
-	</span>
-);
 
 export default class extends Component {
 	constructor( props ) {
---
+++ components/menu.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default ( { href, label } ) => (
+	<span>
+		<Link href={href}><a>{label}</a></Link>
+
+		<style jsx>{`
+			{
+				padding: 6px 4px;
+			};
+
+			a {
+				color: #fff;
+				text-decoration: none;
+				font-size: 16px;
+			};
+		`}</style>
+	</span>
+);
