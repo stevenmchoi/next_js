05748c5c8d750522b026ee4eead5d64279c1cf7e
Steven Choi <steven.m.choi@gmail.com>
2018-06-24 16:05:30 -0700

    Set assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+	assetPrefix: isProd ? 'https://stevenmchoi.github.io/next_js' : '',
 	exportPathMap: function ( defaultPathMap ) {
 		return {
 			'/': { page: '/' },
