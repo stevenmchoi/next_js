defd1dddf3519cf1f91709d176de2c5d5ea1fed2
Steven Choi <steven.m.choi@gmail.com>
2018-06-23 23:56:13 -0700

    Move html and head and body tags to index.html

--- components/page.js
+++ components/page.js
@@ -5,24 +5,15 @@ import calcNewEmployeeData from "../util/employee_data";
 export default class extends Component {
 	render() {
 		return (
-			<html lang="en">
-				<head>
-					<meta charSet="utf-8" />
-					<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-					<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossOrigin="anonymous" />
+			<main>
+				<nav>
+					<Menu href={'/'} label={'New Employee'} />
+					<Menu key='employees' href={'/employees'} label={'Employees'} />
+				</nav>
 
-					<title>Employee Paystubs</title>
-				</head>
-				<body>
-					<main>
-						<nav>
-							<Menu href={'/'} label={'New Employee'} />
-							<Menu key='employees' href={'/employees'} label={'Employees'} />
-						</nav>
+				{this.props.children}
 
-						{this.props.children}
-
-						<style global jsx>{`
+				<style global jsx>{`
 							body {
 								font-family: Helvetica, sans-serif;
 							};
@@ -59,13 +50,7 @@ export default class extends Component {
 								box-sizing: border-box;
 							};
 						`}</style>
-					</main>
-
-					<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-					<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossOrigin="anonymous"></script>
-					<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossOrigin="anonymous"></script>
-				</body>
-			</html >
+			</main>
 		);
 	}
 }
